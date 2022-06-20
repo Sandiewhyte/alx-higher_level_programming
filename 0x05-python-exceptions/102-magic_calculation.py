227641d85bc4f723e68d5d3e7536924a2b0ca2da
Sandiewhyte <isianyasandra.is@gmail.com>
2022-06-20 15:34:05 +0100

    my first python project

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -9,10 +9,10 @@ def magic_calculation(a, b):
         try:
             if i > a:
                 raise Exception('Too far')
-
+            else:
             result += a ** b / i
         except:
             result = b + a
-            break
+            pass
 
     return (result)
