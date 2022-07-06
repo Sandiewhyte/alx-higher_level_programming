5f845ed4f4387667e1efe8cbffbfbef2f60215f8
Sandiewhyte <isianyasandra.is@gmail.com>
2022-07-06 23:30:48 +0100

    my first python project

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""Module that read a file and print your content
-"""
+"""this module writes to a file"""
 
 
-def number_of_lines(filename=""):
-        """Function that read a file and count number of lines.
-
-        Args:
-            filename (str, optional): file path. Defaults to "".
-        """
-        with open(filename, mode='r', encoding='utf-8') as afile:
-                num_line = 0
-                while (afile.readline()):
-                        num_line += 1
-                return num_line
+def write_file(filename="", text=""):
+    """writes a string to a text file and returns
+    the number of characters written"""
+    with open(filename, encoding="utf-8", mode="w+") as f:
+        return f.write(text)
