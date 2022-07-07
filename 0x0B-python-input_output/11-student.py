43a78e5bfd76d10b41a62e517e4db64a1e168951
Sandiewhyte <isianyasandra.is@gmail.com>
2022-07-07 09:13:07 +0100

    my first python project

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -4,19 +4,33 @@
 
 
 class Student:
-        """Class
+    """Class
+    """
+    def __init__(self, first_name, last_name, age):
+        """Constructor
         """
-        def __init__(self, first_name, last_name, age):
-                """Constructor
-                """
-                self.first_name = first_name
-                self.last_name = last_name
-                self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-        def to_json(self):
-                """Method
+    def to_json(self, attrs=None):
+        """Method
+        """
+        if type(attrs) is list:
+            aux_dict = {}
+            for x in attrs:
+                if type(x) is str and x in self.__dict__.keys():
+                    aux_dict[x] = self.__dict__.get(x)
+            return aux_dict
+        else:
+            return self.__dict__
 
-                Returns:
-                    [type]: dict
-                """
-                return self.__dict__
+    def reload_from_json(self, json):
+        """[summary]
+
+        Args:
+            json ([type]): dict
+        """
+        alist_keys = list(json.keys())
+        for key in alist_keys:
+            self.__dict__[key] = json.get(key)
