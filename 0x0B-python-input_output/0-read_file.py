3b48fb5ab70faf70b610acbcc107b826adda4975
Sandiewhyte <isianyasandra.is@gmail.com>
2022-07-07 08:42:39 +0100

    my first python project

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,10 +7,3 @@ def read_file(filename=""):
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end="")
-
-read_file('check')
-
-
-# with open('filename', encoding="utf-8") as f:
-#     for line in f:
-#         print(line, end="")
