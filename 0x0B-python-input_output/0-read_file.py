4a222280c4e2f015924fca8b19075d698c0cfef2
Sandiewhyte <isianyasandra.is@gmail.com>
2022-07-07 08:44:07 +0100

    my first python project

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
-"""module to read a text file"""
-
-
 def read_file(filename=""):
     """reads a text file and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
